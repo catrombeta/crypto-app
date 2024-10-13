0689448c414e782a0bdfc3ee60c7e74388939e4a
Caroline Trombeta <carolinecampost@gmail.com>
2024-10-13 19:14:02 +0100

    feat: new style dependency, as well configuration of tailwin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <p>
-      
-    </p>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <p className="text-3xl font-bold text-blue-500">Hello, Tailwind!</p>
+    </div>
   )
 }
 
